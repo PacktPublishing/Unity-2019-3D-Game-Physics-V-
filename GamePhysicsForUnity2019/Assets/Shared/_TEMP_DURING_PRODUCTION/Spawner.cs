850e2463e837b909eb41966d55c3608a4136d9f8
Samuel Asher Rivello <srivello@hotmail.com>
2019-06-03 00:07:52 -0500

    Update

--- GamePhysicsForUnity2019/Assets/Shared/_TEMP_DURING_PRODUCTION/Spawner.cs
+++ GamePhysicsForUnity2019/Assets/Shared/_TEMP_DURING_PRODUCTION/Spawner.cs
@@ -5,33 +5,51 @@ namespace RMC.UnityGamePhysics.Shared
 {
 	public class Spawner : MonoBehaviour
 	{
-		public float spawnTime = 3f;            // How long between each spawn.
-		public GameObject prefab;                // The enemy prefab to be spawned.
-		public Transform parent;
-		public Transform origin;
-		public Vector3 originOffsetRandom;
+		[SerializeField]
+		private int _spawnAmount = 1;		// How amount per spawn
 
+		[SerializeField]
+		private float _spawnTime = 3f;		// How long between each spawn
 
-		void Start()
+		[SerializeField]
+		private GameObject _prefab;			// The enemy prefab to be spawned
+
+		[SerializeField]
+		private Transform _parent;
+
+		[SerializeField]
+		private Transform _origin;
+
+		[SerializeField]
+		private Vector3 _originOffsetRandom;
+
+		protected void Start()
 		{
-			// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-			InvokeRepeating("Spawn", spawnTime, spawnTime);
+			// Call the Spawn function after a delay of the spawnTime and
+			// then continue to call after the same amount of time
+			InvokeRepeating("Spawn", _spawnTime, _spawnTime);
 
 			// Also spawn one immediatly
 			Spawn();
 		}
 
-		void Spawn()
+		private void Spawn()
 		{
-			// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-			GameObject spawned = Instantiate<GameObject>(prefab);
-			spawned.transform.SetParent(parent, true);
-			spawned.transform.position = origin.position;
+			for (int i = 0; i < _spawnAmount; i++)
+			{
+				// Create an instance of the enemy prefab at the randomly 
+				// selected spawn point's position and rotation.
+				GameObject spawned = Instantiate<GameObject>(_prefab);
+				spawned.transform.SetParent(_parent, true);
+				spawned.transform.position = _origin.position;
 
-			// Randomize starting position
-			float x = Random.RandomRange(-originOffsetRandom.x, originOffsetRandom.x);
-			float y = Random.RandomRange(-originOffsetRandom.y, originOffsetRandom.y);
-			spawned.transform.Translate(new Vector3(x, y, 0));
+				// Randomize starting position
+				float x = Random.Range(-_originOffsetRandom.x, _originOffsetRandom.x);
+				float y = Random.Range(-_originOffsetRandom.y, _originOffsetRandom.y);
+				float z = Random.Range(-_originOffsetRandom.z, _originOffsetRandom.z);
+				spawned.transform.Translate(new Vector3(x, y, z));
+			}
+			
 		}
 	}
 }
