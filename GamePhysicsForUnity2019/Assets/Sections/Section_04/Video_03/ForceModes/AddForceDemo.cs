350d01e419ec517973045aa667572ffe94b3b145
Samuel Asher Rivello <srivello@hotmail.com>
2019-06-04 17:04:36 -0500

    Update

---
+++ GamePhysicsForUnity2019/Assets/Sections/Section_04/Video_03/ForceModes/AddForceDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace RMC.UnityGamePhysics.Sections.Section04
+{
+	public class AddForceDemo : MonoBehaviour
+	{
+		[SerializeField]
+		private Rigidbody _rigidbody;
+
+		[SerializeField]
+		private Vector3 _force;
+
+		[SerializeField]
+		private ForceMode _forceMode;
+
+		protected void FixedUpdate()
+		{
+			if (Input.GetKey (KeyCode.UpArrow))
+			{
+				_rigidbody.AddForce(_force, _forceMode);
+			}
+		}
+	}
+}
