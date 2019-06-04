c60ee991b583b096b105b38ce23f73c9b1f9ef06
Samuel Asher Rivello <srivello@hotmail.com>
2019-06-04 18:46:10 -0500

    Update

---
+++ GamePhysicsForUnity2019/Assets/Sections/Section_04/Video_03/AddTorqueDemo/AddTorqueDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace RMC.UnityGamePhysics.Sections.Section04
+{
+	public class AddTorqueDemo : MonoBehaviour
+	{
+		[SerializeField]
+		private Rigidbody _rigidbody;
+
+		[SerializeField]
+		private KeyCode _posForceKeyCode = KeyCode.UpArrow;
+
+		[SerializeField]
+		private KeyCode _negForceKeyCode = KeyCode.DownArrow;
+
+		[SerializeField]
+		private Vector3 _force;
+
+		[SerializeField]
+		private ForceMode _forceMode;
+
+		protected void FixedUpdate()
+		{
+			if (Input.GetKey (_posForceKeyCode))
+			{
+				_rigidbody.AddTorque(_force, _forceMode);
+			}
+
+			if (Input.GetKey(_negForceKeyCode))
+			{
+				_rigidbody.AddTorque( - _force, _forceMode);
+			}
+		}
+	}
+}
