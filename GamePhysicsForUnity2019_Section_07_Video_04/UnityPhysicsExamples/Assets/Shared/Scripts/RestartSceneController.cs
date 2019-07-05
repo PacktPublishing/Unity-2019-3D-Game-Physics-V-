9b45d41f1405fbf3a6a8450bc7874e742ef846df
Samuel Asher Rivello <srivello@hotmail.com>
2019-07-05 13:33:16 -0500

    Add Dots

---
+++ GamePhysicsForUnity2019_Section_07_Video_04/UnityPhysicsExamples/Assets/Shared/Scripts/RestartSceneController.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RMC.UnityGamePhysics.Shared
+{
+	/// <summary>
+	/// Press the Spacebar to restart the scene.
+	/// </summary>
+	public class RestartSceneController : MonoBehaviour
+	{
+		protected void Update()
+		{
+			
+			if (Input.GetKey(KeyCode.Space))
+			{
+                // Restart DOTS		------------------------------------
+                if (World.Active != null)
+                {
+                    NativeArray<Entity> entities = World.Active.EntityManager.GetAllEntities();
+                    foreach (Entity entity in entities)
+                    {
+                        World.Active.EntityManager.DestroyEntity(entity);
+                    }
+                }
+
+                // Restart Scene		------------------------------------
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+}
