e9153488144bed38c8cf8ec66d615675065895a6
Samuel Asher Rivello <srivello@hotmail.com>
2019-07-09 17:02:00 -0500

    Revert "Revert "Update""
    This reverts commit ebdf62c488e8114da74bd2c750c7a4593e31198f.

--- GamePhysicsForUnity2019/Assets/Sections/Section_05/Video_03/Scripts/UpsetDucksConstants.cs
+++ GamePhysicsForUnity2019/Assets/Sections/Section_05/Video_03/Scripts/UpsetDucksConstants.cs
@@ -2,13 +2,6 @@
 {
 	public static class UpsetDucksConstants 
 	{
-		// Sounds
-		public const int ShootAsteroidSound = 0;
-		public const int WinSound = 1;
-		public const int LoseSound = 2;
-		public const int CollisionSound = 3;
-		public const int ExplosionSound = 4;
-
 		// Tags
 		public const string UpsetDuckTag = "UpsetDuck";
 
@@ -25,7 +18,6 @@
 		public const int PointsPerUpsetDuck = 1;
 		public const float UpsetDuckSpriteFlickerDelay = 0.5f;
 
-		public const float ExplosionDestroyDelay = 0.25f;
 		public const float CrateDestroyDelay = 0;
 	}
 }
