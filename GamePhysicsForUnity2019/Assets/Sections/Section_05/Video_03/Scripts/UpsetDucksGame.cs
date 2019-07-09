f8651d8e53a211e43e7654b6673b26528c603f5a
Samuel Asher Rivello <srivello@hotmail.com>
2019-07-09 13:22:49 -0500

    Update 01

---
+++ GamePhysicsForUnity2019/Assets/Sections/Section_05/Video_03/Scripts/UpsetDucksGame.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace RMC.UnityGamePhysics.Sections.Section05.Video03
+{
+	public class UpsetDucksGame : MonoBehaviour
+	{
+		/// <summary>
+		/// Setup "Singleton" Design Pattern
+		/// See http://bit.ly/Unity_Singleton
+		/// </summary>
+		private static UpsetDucksGame _instance;
+		public static UpsetDucksGame Instance { get { return _instance; } }
+
+		public int Score
+		{
+			get
+			{
+				return _score;
+			}
+			set
+			{
+				_score = value;
+			}
+		}
+
+		public int Asteroids
+		{
+			get
+			{
+				return _asteroids;
+			}
+			set
+			{
+				_asteroids = value;
+			}
+		}
+
+		[SerializeField]
+		private GameObject _worldItemParent = null;
+
+		public List<WorldItem> _worldItems = new List<WorldItem>();
+
+		private int _asteroids = 0;
+		private int _score = 0;
+		private bool _isGameOver = false;
+		private int _upsetDuckCount = 0;
+		private Asteroid _currentAsteroid = null;
+
+		protected void Awake()
+		{
+			_instance = this;
+		}
+
+		protected void Start()
+		{
+			
+			Physics.sleepThreshold = UpsetDucksConstants.PhysicsSleepThreshold;
+
+			// Reset values
+			_isGameOver = false;
+			_currentAsteroid = null;
+			_upsetDuckCount = 0;
+
+			// Create new list of worldItems
+			_worldItems = _worldItemParent.GetComponentsInChildren<WorldItem>().ToList();
+			foreach (WorldItem worldItem in _worldItems)
+			{
+				if (worldItem.gameObject.tag == UpsetDucksConstants.UpsetDuckTag)
+				{
+					// Count how many upsetducks so we know how many we must 'hit'
+					_upsetDuckCount++;
+				}
+			}
+
+			Score = 0;
+			Asteroids = UpsetDucksConstants.MaxAsteroidsPerGame;
+
+			// Start the catapult
+			AddAsteroid();
+		}
+
+		protected void Update()
+		{
+			if (_isGameOver)
+			{
+				return;
+			}
+
+			if (_currentAsteroid != null)
+			{
+				if (_currentAsteroid.IsReleased && 
+					_currentAsteroid.Rigidbody2D.IsSleeping())
+				{
+					_currentAsteroid = null;
+					AddAsteroid();
+				}
+			}
+
+			foreach (WorldItem worldItem in _worldItems)
+			{
+				if (worldItem.gameObject.tag == UpsetDucksConstants.UpsetDuckTag)
+				{
+					
+					if (worldItem.IsAlive && worldItem.Health <= 0)
+					{
+						worldItem.IsAlive = false;
+						Score += UpsetDucksConstants.PointsPerUpsetDuck;
+					}
+				}
+			}
+
+			if (Score >= _upsetDuckCount)
+			{
+				_isGameOver = true;
+			}
+		}
+
+		private void AddAsteroid()
+		{
+			if (Asteroids > 0)
+			{
+				if (Catapult.Instance != null)
+				{
+					_currentAsteroid = Catapult.Instance.AddAsteroid();
+				}
+			}
+			else
+			{
+				_isGameOver = true;
+			}
+		}
+
+		public void DestroyCrate(Crate crate)
+		{
+			_worldItems.Remove(crate.gameObject.GetComponent<WorldItem>());
+
+			StartCoroutine(DestroyGameObjectAfterXSeconds(crate.gameObject, 
+				UpsetDucksConstants.CrateDestroyDelay));
+		}
+
+		private IEnumerator DestroyGameObjectAfterXSeconds(GameObject go, float seconds)
+		{
+			yield return new WaitForSeconds(seconds);
+			Destroy(go);
+		}
+	}
+}
